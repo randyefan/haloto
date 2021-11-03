0bdccdc502247480c34e417deb3e0b877b5be0e4
Darindra R <darindra23@gmail.com>
2021-11-04 01:20:12 +0700

    Fixin Upcoming Cell

--- Table View/UpcomingMaintenance/Components/UpcomingMaintenanceDescription.swift
+++ Table View/UpcomingMaintenance/Components/UpcomingMaintenanceDescription.swift
@@ -26,23 +26,43 @@ class UpcomingMaintenanceDescription: ASCellNode {
     }()
 
     init(model: UpcomingMaintenance) {
-        titleNode.attributedText = .font("\(model.nextServiceOdometer) KM Service", size: 14, fontWeight: .medium, color: UIColor.black, alignment: .right, isTitle: false)
+        titleNode.attributedText = .font(
+            "\(model.nextServiceOdometer) KM Service",
+            size: 14,
+            fontWeight: .medium
+        )
 
-        subTitleNode.attributedText = .font("Check \(model.components.first?.name ?? "") and \(model.components.count - 1) other", size: 12, fontWeight: .regular, color: UIColor.lightGray, alignment: .right, isTitle: false)
+        subTitleNode.attributedText = .font(
+            "Check \(model.components.first?.name ?? "") and \(model.components.count - 1) other",
+            size: 12,
+            color: UIColor.lightGray
+        )
 
-        dateNode.attributedText = .font(model.nextServiceDate, size: 11, fontWeight: .medium, color: UIColor.black, alignment: .right, isTitle: false)
+        dateNode.attributedText = .font(
+            model.nextServiceDate,
+            size: 11,
+            fontWeight: .medium
+        )
         super.init()
         automaticallyManagesSubnodes = true
     }
 
     override func layoutSpecThatFits(_: ASSizeRange) -> ASLayoutSpec {
-        let stack = ASStackLayoutSpec(direction: .vertical,
-                                      spacing: 5,
-                                      justifyContent: .start,
-                                      alignItems: .start,
-                                      children: [titleNode, subTitleNode])
+        let stack = ASStackLayoutSpec(
+            direction: .vertical,
+            spacing: 0,
+            justifyContent: .start,
+            alignItems: .start,
+            children: [titleNode, subTitleNode]
+        )
 
-        let finalStack = ASStackLayoutSpec(direction: .vertical, spacing: 10, justifyContent: .start, alignItems: .start, children: [stack, dateNode])
+        let finalStack = ASStackLayoutSpec(
+            direction: .vertical,
+            spacing: 10,
+            justifyContent: .start,
+            alignItems: .start,
+            children: [stack, dateNode]
+        )
 
         return ASInsetLayoutSpec(insets: UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 16), child: finalStack)
     }
