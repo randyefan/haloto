7729b31976fa0b161b09e171cd0ba346fa59f768
Darindra R <darindra23@gmail.com>
2021-11-03 12:01:28 +0700

    Fixin format

--- Haloto/Vendor/Extensions/Extension+NSAttributedString.swift
+++ Haloto/Vendor/Extensions/Extension+NSAttributedString.swift
@@ -8,14 +8,22 @@
 import Foundation
 import UIKit
 
-private let poppinsBold = UIFont(name: "Poppins-Bold", size: 1) ?? UIFont.systemFont(ofSize: 1)
-private let poppinsRegular = UIFont(name: "Poppins-Regular", size: 1) ?? UIFont.systemFont(ofSize: 1)
-private let poppinsMedium = UIFont(name: "Poppins-Medium", size: 1) ?? UIFont.systemFont(ofSize: 1)
+public enum fontWeight {
+    case bold
+    case medium
+    case regular
 
-private let headingSize: CGFloat = 18
-private let bodySize: CGFloat = 12
-private let captionSize: CGFloat = 11
-
+    var fontWeight: UIFont {
+        switch self {
+        case .bold:
+            return UIFont(name: "Poppins-Bold", size: 1) ?? UIFont.systemFont(ofSize: 1)
+        case .medium:
+            return UIFont(name: "Poppins-Medium", size: 1) ?? UIFont.systemFont(ofSize: 1)
+        case .regular:
+            return UIFont(name: "Poppins-Regular", size: 1) ?? UIFont.systemFont(ofSize: 1)
+        }
+    }
+}
 
 extension NSAttributedString {
     internal class func setFont(font: UIFont, kerning: Double = 0, color: UIColor, lineSpacing: CGFloat? = nil, alignment: NSTextAlignment) -> [NSAttributedString.Key: Any] {
@@ -33,58 +41,14 @@ extension NSAttributedString {
         return attribute
     }
 
-    public class func heading(_ string: String, color: UIColor = .black, alignment: NSTextAlignment = .left) -> NSAttributedString {
+    public class func font(_ string: String, size: CGFloat, fontWeight: fontWeight = .regular, color: UIColor = .black, alignment: NSTextAlignment = .left, isTitle: Bool = false) -> NSAttributedString {
         let attribute = NSAttributedString.setFont(
-            font: poppinsBold.withSize(headingSize),
+            font: fontWeight.fontWeight.withSize(size),
+            kerning: isTitle ? 0 : 0.5,
             color: color,
             lineSpacing: 0,
             alignment: alignment
         )
-        let attributeString = NSMutableAttributedString(string: string, attributes: attribute)
-        return attributeString
-    }
-
-    public class func body(_ string: String,
-        color: UIColor = .black,
-        alignment: NSTextAlignment = .left) -> NSAttributedString {
-        let attribute = NSAttributedString.setFont(
-            font: poppinsRegular.withSize(bodySize),
-            kerning: 16,
-            color: color,
-            lineSpacing: 0,
-            alignment: alignment
-        )
-
-        let attributeString = NSMutableAttributedString(string: string, attributes: attribute)
-        return attributeString
-    }
-
-    public class func caption(_ string: String,
-        color: UIColor = .black,
-        alignment: NSTextAlignment = .left) -> NSAttributedString {
-        let attribute = NSAttributedString.setFont(
-            font: poppinsMedium.withSize(captionSize),
-            kerning: 16,
-            color: color,
-            lineSpacing: 0,
-            alignment: alignment
-        )
-
-        let attributeString = NSMutableAttributedString(string: string, attributes: attribute)
-        return attributeString
-    }
-
-    public class func button(_ string: String,
-        color: UIColor = .black,
-        alignment: NSTextAlignment = .left) -> NSAttributedString {
-        let attribute = NSAttributedString.setFont(
-            font: poppinsRegular.withSize(captionSize),
-            kerning: 16,
-            color: color,
-            lineSpacing: 0,
-            alignment: alignment
-        )
-
         let attributeString = NSMutableAttributedString(string: string, attributes: attribute)
         return attributeString
     }
