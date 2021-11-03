e012c9d43e262764a9b2b00a938e3c8f5fa17884
Darindra R <darindra23@gmail.com>
2021-11-03 23:22:54 +0700

    Fix Centerin Title

--- Haloto/Module/Example/ExampleViewController.swift
+++ Haloto/Module/Example/ExampleViewController.swift
@@ -9,19 +9,37 @@ import UIKit
 import AsyncDisplayKit
 
 class ExampleViewController: ASDKViewController<ASDisplayNode> {
-    
+
+    var button: SmallYellowButtonNode!
     // MARK: - Initializer (Required)
-    
     override init() {
         super.init(node: ASDisplayNode())
+
+        button = SmallYellowButtonNode(title: "Hai Randy", target: self, function: #selector(test))
+
+        node.automaticallyManagesSubnodes = true
+        node.backgroundColor = .white
+
+        node.layoutSpecBlock = { _, _ in
+            return ASInsetLayoutSpec(
+                insets: UIEdgeInsets(top: .infinity, left: .infinity, bottom: .infinity, right: .infinity),
+                child: self.button
+            )
+        }
     }
-    
+
+    @objc func test() {
+        let vc = ListPopupViewController(state: .model)
+        vc.modalPresentationStyle = .fullScreen
+        self.present(vc, animated: true)
+    }
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
     // MARK: - ViewController Lifecycle
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         node.backgroundColor = .blue
--- Haloto/Vendor/Reusable View/Pop Up/List Popup/Header/HeaderListPopUpNode.swift
+++ Haloto/Vendor/Reusable View/Pop Up/List Popup/Header/HeaderListPopUpNode.swift
@@ -12,81 +12,75 @@ import UIKit
 
 class HeaderListPopUpNode: ASDisplayNode {
     // MARK: - Variable
-    
-    private let imageBack: ASDisplayNode = {
+    private let imageBackNode: ASDisplayNode = {
         let node = ASImageNode()
         node.image = UIImage(named: "x_icon")
+        node.style.preferredSize = CGSize(width: 36, height: 36)
         return node
     }()
-    
-    private let title: ASTextNode2 = {
-        var textNode = ASTextNode2()
-        textNode.style.flexGrow = 1
-        return textNode
+
+    private let titleNode: ASTextNode2 = {
+        var node = ASTextNode2()
+        node.style.flexGrow = 1
+        return node
     }()
-    
+
     private var yellowButton: SmallYellowButtonNode?
     private var blueOutlineButton: SmallOutlineButtonNode?
-    
-    private let state: PopUpListState
-    
+
     // MARK: - Initializer
-    
     init(state: PopUpListState) {
-        self.state = state
-        
-        title.attributedText = .font("\(state.rawValue)", size: 18, fontWeight: .bold, color: .black, alignment: .center, isTitle: true)
-        
+        titleNode.attributedText = .font(
+            "\(state.rawValue)",
+            size: 18,
+            fontWeight: .bold,
+            alignment: .center,
+            isTitle: true
+        )
+
         super.init()
-        
+        setupButton(state: state)
         automaticallyManagesSubnodes = true
-        
-        switch state {
-        case .model:
-            yellowButton = SmallYellowButtonNode(title: "Confirm", target: self, function: #selector(tapButtonRight))
-        case .manufacturer:
-            yellowButton = SmallYellowButtonNode(title: "Confirm", target: self, function: #selector(tapButtonRight))
-        case .service:
-            blueOutlineButton = SmallOutlineButtonNode(title: "Add", target: self, function: #selector(tapButtonRight))
-        case .replaced:
-            blueOutlineButton = SmallOutlineButtonNode(title: "Add", target: self, function: #selector(tapButtonRight))
-        }
-        
-        imageBack.view.onTap {
-            self.tapButtonLeft()
-        }
     }
-    
+
     // MARK: - Selector Function
-    
     @objc func tapButtonRight() {
         print("tapped")
     }
-    
+
     @objc func tapButtonLeft() {
         print("tapped")
     }
-    
-    
+
+
     // MARK: - Layout spec
-    
     override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
-        if let yellowButton = yellowButton {
-            yellowButton.style.height = ASDimension(unit: .points, value: 36)
-        } else if let blueOutlineButton = blueOutlineButton {
-            blueOutlineButton.style.height = ASDimension(unit: .points, value: 36)
-        }
-        
-        imageBack.style.preferredSize = CGSize(width: 26, height: 26)
-        
-        let stackHeader = ASStackLayoutSpec(
+        let stack = ASStackLayoutSpec(
             direction: .horizontal,
             spacing: 16,
             justifyContent: .spaceBetween,
             alignItems: .center,
-            children: [imageBack, title, yellowButton, blueOutlineButton].compactMap { $0 }
+            children: [imageBackNode, yellowButton, blueOutlineButton].compactMap { $0 }
         )
-            
-        return ASInsetLayoutSpec(insets: UIEdgeInsets(top: .infinity, left: 0, bottom: 0, right: 0), child: stackHeader)
+
+        let centerTitle = ASCenterLayoutSpec(centeringOptions: .XY, sizingOptions: [], child: titleNode)
+        let background = ASBackgroundLayoutSpec(child: stack, background: centerTitle)
+
+        return background
+//        return ASInsetLayoutSpec(insets: UIEdgeInsets(top: .infinity, left: 0, bottom: 0, right: 0), child: stackHeader)
+    }
+
+    private func setupButton(state: PopUpListState) {
+        switch state {
+        case .model, .manufacturer:
+            yellowButton = SmallYellowButtonNode(title: "Confirm", target: self, function: #selector(tapButtonRight))
+            yellowButton?.style.height = ASDimensionMake(36)
+            blueOutlineButton = nil
+        default:
+            blueOutlineButton = SmallOutlineButtonNode(title: "Add", target: self, function: #selector(tapButtonRight))
+            blueOutlineButton?.style.height = ASDimensionMake(36)
+            yellowButton = nil
+        }
+
     }
 }
