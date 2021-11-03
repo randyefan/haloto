adac9f24cede4444d2bfe81b2cdc56e891254d0c
Dheo Gildas Varian <dheogildasvarian@gmail.com>
2021-11-03 12:09:44 +0700

    SB-43 (belum selesai)

--- Table View/UpcomingMaintenance/Components/UpcomingMaintenanceDescription.swift
+++ Table View/UpcomingMaintenance/Components/UpcomingMaintenanceDescription.swift
@@ -12,54 +12,48 @@ class UpcomingMaintenanceDescription: ASCellNode {
         let node = ASTextNode2()
         return node
     }()
-    
+
     private let subTitleNode: ASTextNode2 = {
         let node = ASTextNode2()
+        node.maximumNumberOfLines = 2
         return node
     }()
-    
+
     private let dateNode: ASTextNode2 = {
         let node = ASTextNode2()
+        node.style.alignSelf = .end
         return node
     }()
-    
+
     override init() {
         titleNode.attributedText = NSAttributedString(string: "10.000 km Service",
                                                       attributes: [
-                                                        .font: UIFont.systemFont(ofSize: 14, weight: .medium)
+                                                          .font: UIFont.systemFont(ofSize: 14, weight: .medium),
                                                       ])
-        
-        subTitleNode.attributedText = NSAttributedString(string: "Check accu health and 2 others",
-                                                      attributes: [
-                                                        .font: UIFont.preferredFont(forTextStyle: .body),
-                                                        .foregroundColor: UIColor.lightGray
-                                                      ])
-        
+
+        subTitleNode.attributedText = NSAttributedString(string: "Check accu health and 2 other",
+                                                         attributes: [
+                                                             .font: UIFont.systemFont(ofSize: 12),
+                                                             .foregroundColor: UIColor.lightGray,
+                                                         ])
+
         dateNode.attributedText = NSAttributedString(string: "This Week",
                                                      attributes: [
-                                                        .font: UIFont.preferredFont(forTextStyle: .caption1)
+                                                         .font: UIFont.preferredFont(forTextStyle: .caption1),
                                                      ])
         super.init()
         automaticallyManagesSubnodes = true
     }
-    
-    override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
+
+    override func layoutSpecThatFits(_: ASSizeRange) -> ASLayoutSpec {
         let stack = ASStackLayoutSpec(direction: .vertical,
                                       spacing: 5,
                                       justifyContent: .start,
                                       alignItems: .start,
                                       children: [titleNode, subTitleNode])
+
+        return ASStackLayoutSpec(direction: .vertical, spacing: 10, justifyContent: .start, alignItems: .start, children: [stack, dateNode])
         
-        let inset = ASInsetLayoutSpec(insets: UIEdgeInsets(top: 90,
-                                                           left: 50,
-                                                           bottom: 5,
-                                                           right: 5),
-                                      child: dateNode)
         
-        return ASStackLayoutSpec(direction: .vertical,
-                                 spacing: 5,
-                                 justifyContent: .start,
-                                 alignItems: .start,
-                                 children: [stack, inset])
     }
 }
--- Table View/UpcomingMaintenance/UpcomingMaintenanceCell.swift
+++ Table View/UpcomingMaintenance/UpcomingMaintenanceCell.swift
@@ -8,54 +8,52 @@
 import AsyncDisplayKit
 import UIKit
 
-class UpcomingMaintenanceCell: ASCellNode{
-    
+class UpcomingMaintenanceCell: ASCellNode {
     private let countUpcomingMaintenanceNode: ASTextNode2 = {
-       let node = ASTextNode2()
+        let node = ASTextNode2()
         return node
     }()
-    
+
     private let upcomingMaintenanceNode: ASTextNode2 = {
-       let node = ASTextNode2()
+        let node = ASTextNode2()
         return node
     }()
-    
+
     private let backgroundNode: ASDisplayNode = {
-       let node = ASDisplayNode()
+        let node = ASDisplayNode()
         return node
     }()
-    
+
     let upcomingMaintenanceDescription: UpcomingMaintenanceDescription
-    
 
     override init() {
         countUpcomingMaintenanceNode.attributedText = NSAttributedString(string: "3",
                                                                          attributes: [
-                                                                            .font: UIFont.systemFont(ofSize: 45, weight: .semibold),
-                                                                            .foregroundColor: UIColor.white
+                                                                             .font: UIFont.systemFont(ofSize: 45, weight: .semibold),
+                                                                             .foregroundColor: UIColor.white,
                                                                          ])
-        
+
         upcomingMaintenanceNode.attributedText = NSAttributedString(string: "Components",
                                                                     attributes: [
                                                                         .font: UIFont.systemFont(ofSize: 9, weight: .regular),
-                                                                        .foregroundColor: UIColor.white
+                                                                        .foregroundColor: UIColor.white,
                                                                     ])
-        
+
         backgroundNode.style.preferredSize = CGSize(width: 84, height: 78)
         backgroundNode.backgroundColor = UIColor(red: 126 / 255.0,
                                                  green: 161 / 255.0,
                                                  blue: 214 / 255.0,
                                                  alpha: 1.0)
-        
+
         upcomingMaintenanceDescription = UpcomingMaintenanceDescription()
-        
+
         super.init()
         automaticallyManagesSubnodes = true
         backgroundColor = .white
         setShadow()
     }
-    
-    func setShadow(){
+
+    func setShadow() {
         clipsToBounds = false
         cornerRadius = 8
         shadowColor = UIColor.black.cgColor
@@ -63,14 +61,12 @@ class UpcomingMaintenanceCell: ASCellNode{
         shadowOffset.height = 2
         shadowRadius = 4
     }
-    
-    override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
-        let stack = ASStackLayoutSpec(direction: .vertical, spacing: 5, justifyContent: .start, alignItems: .start, children: [countUpcomingMaintenanceNode, upcomingMaintenanceNode])
-        
-//        let inset = ASInsetLayoutSpec(insets: UIEdgeInsets(top: .infinity, left: .infinity, bottom: .infinity, right: .infinity), child: stack)
-        
-        let backgroundInset = ASBackgroundLayoutSpec(child: stack, background: backgroundNode)
-        
-        return ASStackLayoutSpec(direction: .horizontal, spacing: 5, justifyContent: .start, alignItems: .start, children: [backgroundInset, upcomingMaintenanceDescription])
+
+    override func layoutSpecThatFits(_: ASSizeRange) -> ASLayoutSpec {
+        let stack = ASStackLayoutSpec(direction: .vertical, spacing: -5, justifyContent: .center, alignItems: .center, children: [countUpcomingMaintenanceNode, upcomingMaintenanceNode])
+
+        let backgroundInset = ASOverlayLayoutSpec(child: backgroundNode, overlay: stack)
+
+        return ASStackLayoutSpec(direction: .horizontal, spacing: 5, justifyContent: .start, alignItems: .center, children: [backgroundInset, upcomingMaintenanceDescription])
     }
 }
