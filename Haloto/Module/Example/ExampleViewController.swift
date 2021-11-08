7c67cc87bdfa68900f32a3decf9e7ad044fd437b
Darindra R <darindra23@gmail.com>
2021-11-09 00:03:12 +0700

    Fix example file

--- Haloto/Module/Example/ExampleViewController.swift
+++ Haloto/Module/Example/ExampleViewController.swift
@@ -5,15 +5,13 @@ import AsyncDisplayKit
 class ExampleViewController: ASDKViewController<ASDisplayNode> {
 
     override init() {
-        let card = ConsultRatingNode()
-
         super.init(node: ASDisplayNode())
         node.automaticallyManagesSubnodes = true
         node.backgroundColor = .white
-        node.layoutSpecBlock = { _, _ in
-            ASInsetLayoutSpec(insets: UIEdgeInsets(top: .infinity, left: 16, bottom: .infinity, right: 16), child: card)
-
-        }
+//        node.layoutSpecBlock = { _, _ in
+//            ASInsetLayoutSpec(insets: UIEdgeInsets(top: .infinity, left: 16, bottom: .infinity, right: 16), child: card)
+//
+//        }
     }
 
     required init?(coder: NSCoder) {
