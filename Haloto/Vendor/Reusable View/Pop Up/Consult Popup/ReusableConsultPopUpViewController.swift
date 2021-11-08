344548089d0a52493f8aa9ebe704901438b7863c
Randy Efan Jayaputra <randyrandyrej8@gmail.com>
2021-11-09 01:33:46 +0700

    stitching some module

--- Haloto/Vendor/Reusable View/Pop Up/Consult Popup/ReusableConsultPopUpViewController.swift
+++ Haloto/Vendor/Reusable View/Pop Up/Consult Popup/ReusableConsultPopUpViewController.swift
@@ -12,8 +12,8 @@ class ReusableConsultPopUpViewController: ASDKViewController<ASDisplayNode> {
     
     let buttonXNode: ASImageNode = {
         let node = ASImageNode()
-        node.style.width = ASDimension(unit: .points, value: 26)
-        node.style.height = ASDimension(unit: .points, value: 26)
+        node.style.width = ASDimension(unit: .points, value: 30)
+        node.style.height = ASDimension(unit: .points, value: 30)
         node.image = UIImage(named: "x_icon")
         return node
     }()
@@ -27,10 +27,12 @@ class ReusableConsultPopUpViewController: ASDKViewController<ASDisplayNode> {
         super.init(node: ASDisplayNode())
         node.automaticallyManagesSubnodes = true
         
+        tapObserve()
+        
         node.layoutSpecBlock = { [weak self] _, _ in
             guard let self = self else { return ASLayoutSpec() }
             
-            let imageInset = ASInsetLayoutSpec(insets: UIEdgeInsets(top: .topSafeArea, left: 8, bottom: .infinity, right: .infinity),
+            let imageInset = ASInsetLayoutSpec(insets: UIEdgeInsets(top: .topSafeArea, left: 16, bottom: .infinity, right: .infinity),
                                                child: self.buttonXNode)
             
             let inset = ASInsetLayoutSpec(insets: UIEdgeInsets(top: .infinity, left: 18, bottom: .infinity, right: 18), child: self.centreNode)
@@ -49,5 +51,13 @@ class ReusableConsultPopUpViewController: ASDKViewController<ASDisplayNode> {
     override func viewDidLoad() {
         super.viewDidLoad()
         node.backgroundColor = .white
+    }
+    
+    // MARK: - Tap Observe
+    
+    func tapObserve() {
+        buttonXNode.view.onTap {
+            self.dismiss(animated: true, completion: nil)
+        }
     }
 }
