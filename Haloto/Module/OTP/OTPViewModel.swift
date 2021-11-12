b2828690de8350ec8b5910d2159b3355fe796ca7
Darindra R <darindra23@gmail.com>
2021-11-12 09:50:25 +0700

    Resend otp fix

--- Haloto/Module/OTP/OTPViewModel.swift
+++ Haloto/Module/OTP/OTPViewModel.swift
@@ -10,12 +10,14 @@ import RxCocoa
 import RxSwift
 
 internal class OTPViewModel {
-    private let OTP: Driver<String>
+    private var OTP: Driver<String>
     private let phoneNumber: Driver<String>
+    private let useCase: LoginUseCase
 
-    internal init(OTP: Driver<String>, phoneNumber: Driver<String>) {
+    internal init(OTP: Driver<String>, phoneNumber: Driver<String>, useCase: LoginUseCase = LoginUseCase.live) {
         self.OTP = OTP
         self.phoneNumber = phoneNumber
+        self.useCase = useCase
     }
 
     internal struct Input {
@@ -29,10 +31,30 @@ internal class OTPViewModel {
         internal let signUpDidTap: Driver<Void>
         internal let OTPMatched: Driver<Bool>
         internal let phoneNumber: Driver<String>
+        internal let OTPResent: Driver<String>
     }
 
     internal func transform(input: Input) -> Output {
-        let checkOTP = input.submit.withLatestFrom(input.textFieldTriger)
+        var newOTP = OTPGenerator.shared.getOTP()
+
+        var checkOTP = input.submit.withLatestFrom(input.textFieldTriger)
+            .flatMapLatest { [OTP] otpInput -> Driver<Bool> in
+            return Driver.combineLatest(OTP, Driver.just(otpInput)).map { $0 == $1 }
+        }
+
+        let resendOTP = input.resendOtpDidTap.withLatestFrom(phoneNumber).flatMap { [useCase] phoneNumber -> Driver<WhatsappResponse> in
+            newOTP = OTPGenerator.shared.getOTP()
+            return useCase
+                .sendOTP(phoneNumber.replacingCharacters(in: ...phoneNumber.startIndex, with: "62"), newOTP.message)
+                .compactMap(\.success)
+                .asDriverOnErrorJustComplete()
+        }
+
+        OTP = resendOTP.flatMapLatest { _ -> Driver<String> in
+            Driver.just(newOTP.code)
+        }
+
+        checkOTP = input.submit.withLatestFrom(input.textFieldTriger)
             .flatMapLatest { [OTP] otpInput -> Driver<Bool> in
             return Driver.combineLatest(OTP, Driver.just(otpInput)).map { $0 == $1 }
         }
@@ -40,7 +62,8 @@ internal class OTPViewModel {
         return Output(
             signUpDidTap: input.tapSignUp,
             OTPMatched: checkOTP,
-            phoneNumber: phoneNumber
+            phoneNumber: phoneNumber,
+            OTPResent: OTP
         )
     }
 }
