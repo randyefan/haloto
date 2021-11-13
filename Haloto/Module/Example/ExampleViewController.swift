e8a40e16331507f621c5f902dcc4a8aa72714a1a
Javier <pier9402@gmail.com>
2021-11-13 20:55:16 +0800

    Test

--- Haloto/Module/Example/ExampleViewController.swift
+++ Haloto/Module/Example/ExampleViewController.swift
@@ -39,11 +39,10 @@ class ExampleViewController: ASDKViewController<ASDisplayNode> {
         super.init(node: ASDisplayNode())
         node.automaticallyManagesSubnodes = true
         node.backgroundColor = .white
-        let selectNode = SelectFieldStack(title: "Manufacturer", placeholder: "Choose your vehicle manufacturer")
-        node.layoutSpecBlock = { _, _ in
-            ASInsetLayoutSpec(insets: UIEdgeInsets(top: .infinity, left: 16, bottom: .infinity, right: 16), child: selectNode)
-
-        }
+//        node.layoutSpecBlock = { _, _ in
+//            ASInsetLayoutSpec(insets: UIEdgeInsets(top: .infinity, left: 16, bottom: .infinity, right: 16), child: card)
+//
+//        }
     }
 
     required init?(coder: NSCoder) {
